88ff0701f5ac0e150d3df4e558344447d8146635
camiloamolinas <xcams_95@hotmail.com>
2016-03-09 08:08:26 -0500

    Update RegistroFamiliar.java

--- src/ejercicioenclase_familia/RegistroFamiliar.java
+++ src/ejercicioenclase_familia/RegistroFamiliar.java
@@ -10,5 +10,34 @@ package ejercicioenclase_familia;
  * @author Camilo
  */
 public class RegistroFamiliar {
+        
+    private Familia[] familias;
+
+    public RegistroFamiliar(Familia[] familias) {
+        this.familias = new Familia[5];
+    }
+
+    public Familia[] getFamilias() {
+        return familias;
+    }
     
+    
+    public void verFamilia(String key){
+        HashMap<String, Familia> apellidos = new HashMap<String, Familia>();
+        
+        for(int j=0; j<familias.length; j++){
+            apellidos.put(familias[j].getApellidoFamilia(), familias[j]);
+            
+        }
+        System.out.println("Familia : " + key);
+        Familia Aux = apellidos.get(key);
+        
+        if(Aux != null){
+            System.out.println(Aux);
+          
+        }else{
+            System.out.println("Familia : " + Aux + "Don`t exist");
+        }
+        
+    }
 }
